d390584c13156d233611b0ec4d01a432985d8226
Yervant E. Bastikian <yebastikian@gmail.com>
2018-01-24 18:32:26 -0800

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+    return [
+        {'title': 'JavascriptGG', pages: 101},
+        {'title': 'C++ for nubs', pages: 344},
+        {'title': 'The Virtual Machine', pages: 25}
+    ]
+}
